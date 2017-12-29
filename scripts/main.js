ff07558c7c0084a7b573d4bd8615e0981253b54f
shayalaluf <shayalalu@gmail.com>
2017-12-29 11:39:47 +0200

    add error massage on failing to load products

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,21 @@
 (function () {
-    var productsFileUrl = 'assets/products.json';
+    var productsFileUrl = '../assets/products.json';
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             initializeData(JSON.parse(xhttp.responseText));
         }
+        if (this.readyState === 4 && this.status === 404) {
+            showError();
+        }
     };
     xhttp.open("GET", productsFileUrl, true);
     xhttp.send();
+    var productList = document.querySelector('#products-list');
 
     function initializeData(data) {
         var productsRenderer = new ProductRenderer();
         var shoppingCart = new ShoppingCart();
-        var productList = document.querySelector('#products-list');
         var products = data.map(function (value) {
             return new Product(value, shoppingCart);
         });
@@ -20,6 +23,18 @@
         products.forEach(function (value) {
             productsRenderer.render(value, productList)
         });
+    }
+
+    function showError() {
+        var errorContainer = document.createElement('div');
+        errorContainer.className = 'ui negative message';
+
+        var errorMessage = document.createElement('div');
+        errorMessage.className = 'header';
+        errorMessage.innerText = 'Unable to load product. Please contact your nearest Amaromach branch';
+
+        errorContainer.appendChild(errorMessage);
+        productList.appendChild(errorContainer)
     }
 
     $('.dropdown').dropdown({transition: 'drop'}).dropdown({on: 'hover'});
